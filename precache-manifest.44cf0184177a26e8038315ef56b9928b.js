62b480b57f2042c801e03b6e1e7333d2447f0048
Yaroslav Yadrishnikov <y.yadrushnikov@corp.vk.com>
2019-09-16 13:40:25 +0300

    first commit

---
+++ precache-manifest.44cf0184177a26e8038315ef56b9928b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "684533c40a2f88feaea7",
+    "url": "/vkminiapptest/static/css/main.f661453c.chunk.css"
+  },
+  {
+    "revision": "684533c40a2f88feaea7",
+    "url": "/vkminiapptest/static/js/main.0d925488.chunk.js"
+  },
+  {
+    "revision": "2345b592cf8a0bd3919c",
+    "url": "/vkminiapptest/static/js/runtime~main.8d1a848a.js"
+  },
+  {
+    "revision": "d527038eb896f7e44350",
+    "url": "/vkminiapptest/static/css/2.b281ff74.chunk.css"
+  },
+  {
+    "revision": "d527038eb896f7e44350",
+    "url": "/vkminiapptest/static/js/2.4e344f1c.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkminiapptest/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "f730e9c1504f0df66eb819be6779e4e1",
+    "url": "/vkminiapptest/index.html"
+  }
+];
